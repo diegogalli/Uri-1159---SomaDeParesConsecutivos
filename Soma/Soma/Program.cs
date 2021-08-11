813a1e2275a07f294734adea219d936da762276c
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 15:59:03 -0300

    Uri 1159 Soma de Pares Consecutivos

---
+++ Soma/Soma/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int x = int.Parse(Console.ReadLine());
+            
+            while ( x != 0)
+            {
+                //se for impar, some mais 1
+                if (x % 2 != 0)
+                {
+                    x = x+ 1;
+                }
+
+                //expressão simplificada: 5 * x + 20
+                int soma = x + x + 2 + x + 4 + x + 6 + x + 8;
+                Console.WriteLine(soma);
+
+                x = int.Parse(Console.ReadLine());
+               
+            }
+
+        }
+    }
+}
